e62c58ced38b043b8851ff00db6a285dcde8d6c2
Alexis Koss <alexis30@uw.edu>
2018-02-07 23:57:16 -0800

    added toolbar with alert dialog

--- iQuiz/ViewController.swift
+++ iQuiz/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func settingsPress(_ sender: Any) {
+        let alert = UIAlertController(title: "Settings", message: "Settings go here.", preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .`default`, handler: { _ in
+        }))
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
