b191c66184a6ffccc74e0bd27759166b7d3cc229
Alexis Koss <alexis30@uw.edu>
2018-02-21 16:46:17 -0800

    part 3: finished

--- iQuiz/ViewController.swift
+++ iQuiz/ViewController.swift
@@ -37,128 +37,98 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         self.present(QuestionViewController, animated: true, completion: nil)
     }
 
-    //settings alert
+    
+    //settings alert for inserting a custom json file
     //reference for text field in alert controller https://stackoverflow.com/questions/29808380/swift-insert-alert-box-with-text-input-and-store-text-input
     @IBAction func settingsPress(_ sender: Any) {
         let alert = UIAlertController(title: "Settings", message: "Insert a file you would like to load.", preferredStyle: .alert)
-        
-        /*alert.addAction(UIAlertAction(title: NSLocalizedString("Check now", comment: "Default action"), style: .`default`, handler: { _ in
-        }))*/
         let confirmAction = UIAlertAction(title: "Confirm", style: .default) { (_) in
             let file: String = alert.textFields![0].text!
-            print("FILE ----> " + file)
-            self.url = file
-            //https://raw.githubusercontent.com/alexiskoss/iQuiz/master/questions.json
+            
+            //set the subjects array to empty before populating it with a new file
             self.subjects = []
-            UserDefaults.standard.set(self.url, forKey: "quiz_preference")
+            UserDefaults.standard.set(file, forKey: "quiz_preference")
             self.getJSON(urlString: file)
         }
-        
         let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { (_) in }
         
         alert.addAction(confirmAction)
         alert.addAction(cancelAction)
-        
         alert.addTextField { (textField: UITextField) in
             textField.placeholder = "URL here"
         }
+        
         updateDisplayFromDefaults(alert: alert)
         self.present(alert, animated: true, completion: nil)
     }
     
     //reference for json parsing from url https://stackoverflow.com/questions/42130002/post-data-and-get-data-from-json-url-in-swift
-    //URLSession method
+    //parses the json file to insert the quizzes and questions
     func getJSON(urlString:String){
         let url = URL(string: urlString)
         if Reachability.isConnectedToNetwork() == true {
             URLSession.shared.dataTask(with:url!) { (data, response, error) in
-                if error != nil {
+                if error != nil { //throw error if JSON can't be grabbed from the URL
                     let alert = UIAlertController(title: "Custom quiz file", message: "Download failed.", preferredStyle: .alert)
                     let confirmAction = UIAlertAction(title: "OK", style: .default, handler: nil)
                     alert.addAction(confirmAction)
+                    self.getJSON(urlString: "https://tednewardsandbox.site44.com/questions.json")
                     self.present(alert, animated: true, completion: nil)
                 } else {
                     do {
+                        //get the JSON data
                         let response = try JSONSerialization.jsonObject(with: data!, options: []) as? NSArray
                         let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathComponent("questions.json")
+                        //store the file locally
                         try response?.write(to: path)
                         
-                        let quizDetails = response as? [[String: Any]] ?? []
-                        for subject in quizDetails {
-                            //subjects.append(subject)
-                            //print("SUBJECT!!!!!------> \(String(describing: subject["questions"]))")
-                            let subjectJSON = Subject(subjectName: subject["title"] as! String, subjectDesc: subject["desc"] as! String, questions: [])
-                            let questions = subject["questions"] as! [[String: Any]]
-                            //let questions = arrayOfDetails![0]["questions"] as! [[String: Any]]
-                            for question in questions {
-                                //print("QUESTION!!!!---> \(question)")
-                                subjectJSON.questions.append(Question(questionText: question["text"] as! String, choices: question["answers"] as! [String] , answer: question["answer"] as! String))
-                            }
-                            //print(subjectJSON)
-                            self.subjects.append(subjectJSON)
-                            //print(self.subjects)
-                            //stored JSON locally
-                        }
-                        //store JSON locally
-                        //response!.write(toFile: NSHomeDirectory() + "/Documents/data", atomically: true)
-
-
-                        
-                        print(NSHomeDirectory() + "/Documents")
-                        
-                        DispatchQueue.main.async{
-                            self.tblQuiz.reloadData()
-                        }
-                    } catch let error as NSError {
-                        print(error)
+                        self.iterateJSON(jsonData: response!)
+                    } catch {
+                        //throw an error if the data from the URL cannot be grabbed
+                        let alert = UIAlertController(title: "Custom quiz file", message: "Download failed.", preferredStyle: .alert)
+                        let confirmAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+                        alert.addAction(confirmAction)
+                        self.getJSON(urlString: "https://tednewardsandbox.site44.com/questions.json")
+                        self.present(alert, animated: true, completion: nil)
                     }
                 }
                 }.resume()
-        } else { //not connected to internet
+        } else { //not connected to internet, load local storage json file
             DispatchQueue.main.async{
-                
                 let alert = UIAlertController(title: "No internet connection", message: "Connect your device to the internet.", preferredStyle: .alert)
-                
                 let confirmAction = UIAlertAction(title: "OK", style: .default, handler: nil)
-                
                 alert.addAction(confirmAction)
                 
                 self.loadLocal()
                 self.present(alert, animated: true, completion: nil)
-                
             }
         }
     }
     
+    //loads the locally stored json file
     func loadLocal() {
-        print("yes")
         let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0].appendingPathComponent("questions.json")
         let jsonData = NSArray(contentsOf: path)!
-        print(jsonData);
-        
+
+        iterateJSON(jsonData: jsonData)
+    }
+    
+    func iterateJSON(jsonData: NSArray) {
+        //iterate through the data to populate the table
         let quizDetails = jsonData as? [[String: Any]] ?? []
         for subject in quizDetails {
-            //subjects.append(subject)
-            //print("SUBJECT!!!!!------> \(String(describing: subject["questions"]))")
             let subjectJSON = Subject(subjectName: subject["title"] as! String, subjectDesc: subject["desc"] as! String, questions: [])
             let questions = subject["questions"] as! [[String: Any]]
-            //let questions = arrayOfDetails![0]["questions"] as! [[String: Any]]
             for question in questions {
-                //print("QUESTION!!!!---> \(question)")
                 subjectJSON.questions.append(Question(questionText: question["text"] as! String, choices: question["answers"] as! [String] , answer: question["answer"] as! String))
             }
-            //print(subjectJSON)
             self.subjects.append(subjectJSON)
-            //print(self.subjects)
-            //stored JSON locally
-            
-            DispatchQueue.main.async{
-                self.tblQuiz.reloadData()
-            }
-        
+        }
+        //reload table with quiz data
+        DispatchQueue.main.async{
+            self.tblQuiz.reloadData()
         }
     }
-
     
     //reference for settings bundle https://makeapppie.com/2016/03/14/using-settings-bundles-with-swift/
     func registerSettingsBundle(){
@@ -166,8 +136,8 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         UserDefaults.standard.register(defaults: appDefaults)
     }
     
+    //Get the defaults from the application settings
     func updateDisplayFromDefaults(alert: UIAlertController){
-        //Get the defaults
         let defaults = UserDefaults.standard
         if let quizURL = defaults.string(forKey: "quiz_preference"){
             alert.textFields![0].text! = quizURL
@@ -178,15 +148,17 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         registerSettingsBundle()
-        //UserDefaults.standard.set(url, forKey: "quiz_preference")
+        
+        //get any saved url from the application settings
         if(UserDefaults.standard.string(forKey: "quiz_preference") != nil) {
             url = UserDefaults.standard.string(forKey: "quiz_preference")!
         } else {
             url = "https://tednewardsandbox.site44.com/questions.json"
         }
+        
         getJSON(urlString: url)
-
         
         //load the initial table
         tblQuiz.dataSource = self
